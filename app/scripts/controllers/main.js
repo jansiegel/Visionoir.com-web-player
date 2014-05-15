c07876fa201e3e946285b8efda0fe498fcc35f52
Jan Siegel <kontakt@jansiegel.pl>
2014-05-15 23:17:06 +0200

    Early beta

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,41 @@
 'use strict';
 
 angular.module('visioPlayerApp')
-  .controller('MainCtrl', ['$scope','CanvasGeneratorService', 'DimensionSetupService', function ($scope,canvasGeneratorService,dimSetService) {
+  .controller('MainCtrl', ['$scope','CanvasGeneratorService', 'DimensionSetupService','AudioAnalyzerService', function ($scope,canvasGeneratorService,dimSetService,audioAnalyzerService) {
 
-  $scope.init = function(){
-  	dimSetService.setMainCanvasDimensions();
-    canvasGeneratorService.prepareOffscreen();
-    canvasGeneratorService.preRenderKeyframes(7,2);
+  	$scope.distortionOffset = [9,5];
 
-    canvasGeneratorService.sendKeyframeToView(0);
+  	$scope.update = function() {
+  		// function update() {
+  			requestAnimationFrame($scope.update);
+  			var currentLevel = audioAnalyzerService.getFrequencyData(true);
 
-    // canvasGeneratorService.blendingAdd([0,0],undefined,undefined);
+  			var distortion = parseInt($scope.distortionOffset[0] * (currentLevel / 256) );
 
-  }();
+  			
 
+  			if(!isNaN(distortion)) {
+  				canvasGeneratorService.sendKeyframeToView(distortion);
+  			}
+  			
+	}
+
+  	$scope.init = function(){
+  		dimSetService.setMainCanvasDimensions();
+  		canvasGeneratorService.prepareOffscreen();
+  		canvasGeneratorService.preRenderKeyframes($scope.distortionOffset[0],$scope.distortionOffset[1]);
+
+	    // canvasGeneratorService.sendKeyframeToView(0);
+
+	    audioAnalyzerService.prepareAudioContext();
+
+	    audioAnalyzerService.injectTrack(audioAnalyzerService.providePlaylist()[0]);
+
+	    audioAnalyzerService.setAnalyzer();
+
+
+    	$scope.update();
+
+	}();
 
   }]);
--- app/scripts/services/audioanalyzerservice.js
+++ app/scripts/services/audioanalyzerservice.js
@@ -1,6 +1,61 @@
 'use strict';
 
 angular.module('visioPlayerApp')
-  .service('Audioanalyzerservice', function Audioanalyzerservice() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('AudioAnalyzerService', function AudioAnalyzerService() {
+    	var service = {};
+
+    	service.providePlaylist = function(dir) {
+
+    		//mock
+    		return ['mp3/Visionoir - Je, Noir.mp3'];
+    	}
+
+    	service.injectTrack = function(url) {
+
+    		service.audioElement.setAttribute('src', url);
+
+    		service.audioElement.play();
+    		// service.audioElement.pause();
+
+    	}
+
+    	service.prepareAudioContext = function() {
+    		if(typeof(AudioContext) == "undefined") {
+    			service.context = new webkitAudioContext(); // for chrome
+    		} else {
+    			service.context = new AudioContext(); // for firefox
+    		}
+
+    		service.audioElement = document.createElement('audio');
+    	}
+
+    	service.setAnalyzer = function() {
+    		service.analyser = service.context.createAnalyser();
+    		service.analyser.fftSize = 32;
+    		service.frequencyData = new Uint8Array(service.analyser.frequencyBinCount);
+    		service.analyser.getByteFrequencyData(service.frequencyData);
+
+console.log(service.audioElement)
+
+    		service.audioElement.addEventListener("canplay", function() {
+    			var source = service.context.createMediaElementSource(service.audioElement);
+    			source.connect(service.analyser);
+    			service.analyser.connect(service.context.destination);
+    		});
+    	}
+
+    	service.getFrequencyData = function(reduced) {
+    		service.analyser.getByteFrequencyData(service.frequencyData);
+
+    		if(reduced && service.frequencyData[2] != 0) {
+    			var sum = 0;
+    			for(var i = 0 ; i < service.frequencyData.length ; i++) {
+    				sum += service.frequencyData[i];
+    			}
+    			return sum/service.frequencyData.length;
+    		}
+    		// console.log(service.frequencyData);
+    	}
+
+    	return service;
   });
